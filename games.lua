3ad2029ce3c2c60efa9bdef972b94fa4547310fb
Zypher <zypher728@gmail.com>
2025-05-02 23:31:06 -0700

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [3623096087] = "https://raw.githubusercontent.com/XCL-Zypher/Murderer-Hub/refs/heads/main/ML.lua", -- Muscle Legends
+    [3101667897] = "https://raw.githubusercontent.com/XCL-Zypher/Murderer-Hub/refs/heads/main/LOS.lua", -- Legends of Speed
+}
+
+return Games
