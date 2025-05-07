102d2319d5546d3470eff87ccabcc1f050a83b6b
Zypher <zypher728@gmail.com>
2025-05-07 14:44:45 -0700

    Update and rename Murderer.lua to EpicHub.lua

--- EpicHub.lua
+++ EpicHub.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/XCL-Zypher/Murderer-Hub/refs/heads/main/games.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/XCL-Zypher/Epic-Hub/refs/heads/main/games.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
