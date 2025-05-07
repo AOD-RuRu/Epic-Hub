90f6ddda1b39bd540df7004bf11f8ddd792e83be
Zypher <zypher728@gmail.com>
2025-05-07 16:42:46 -0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [3623096087] = "https://raw.githubusercontent.com/XCL-Zypher/Epic-Hub/refs/heads/main/ML.lua", -- Muscle Legends
+  [3623096087] = "https://raw.githubusercontent.com/DangBroLMAO/bludlol/refs/heads/main/Muscle%20Legends", -- Muscle Legends
     [3101667897] = "https://raw.githubusercontent.com/XCL-Zypher/Epic-Hub/refs/heads/main/LOS.lua", -- Legends of Speed
 }
 
